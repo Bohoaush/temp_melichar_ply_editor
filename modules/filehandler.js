6f7c694dd9c01ccb6ac73d7744e6bbddb48bce89
Bohoaush <sin@boprograms.eu>
2022-12-27 19:56:43 +0100

    adding keyboard controll and fixes

--- modules/filehandler.js
+++ modules/filehandler.js
@@ -13,7 +13,7 @@ function loadPlyFromFile(filename) {
                 var plyItemsArr = [];
                 for (let plyitem of xmlparser.parse(data).playlist.clip) {
                     var jsoitem = {}
-                    jsoitem.path = plyitem['#text'];
+                    jsoitem.path = plyitem['#text'].replace("/mnt/Video/", "");
                     jsoitem.duration = plyitem['@_duration'];
                     plyItemsArr.push(jsoitem);
                     console.log(jsoitem);
@@ -39,7 +39,7 @@ function listAvailablePlys() {
 function writePlyToFile(playlistObj, filename) {
     var exportXml = "<?xml version=\"1.0\"?>\n<playlist>\n"
     for (let plyitem of playlistObj.plyitems) {
-        exportXml += ("   <clip duration=\"" + plyitem.duration + "\">" + plyitem.path + "</clip>\n");
+        exportXml += ("   <clip duration=\"" + Math.trunc(plyitem.duration) + "\">/mnt/Video/" + plyitem.path + "</clip>\n");
     }
     exportXml += "</playlist>";
     fs.writeFile("/mnt/Video/playlisty/" + filename, exportXml, (err) => {
